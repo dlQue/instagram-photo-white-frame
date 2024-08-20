6083947aa21cdbcee802693e822811710deb9b35
dlQue <68498576+dlQue@users.noreply.github.com>
2024-08-20 12:17:16 +0300

    update

--- add white frame for mac and win.js
+++ add white frame for mac and win.js
@@ -1,5 +1,5 @@
-// Modified from ruhua (vx:xiaoaozi2018) 2021/10/26
-// Made by Que, April 11, 2023
+// Modified from ruhua 2021/10/26
+// Made by D.Q., April 11, 2023
 
 alert("Choose your input folder", "Choose your input", false);
 var inputFolder = Folder.selectDialog("Choose your input folder：");
@@ -7,70 +7,43 @@ var inputFolder = Folder.selectDialog("Choose your input folder：");
 alert("Choose your output folder", "Choose your output", false);
 var outputFolder = Folder.selectDialog("Choose your output folder：");
 
-// all files under the input folder, save it as a list
 var fileList = inputFolder.getFiles();
 
-// for all files inside of fileList
-for (var i=0; i<fileList.length; i++) {
+for (var i = 0; i < fileList.length; i++) {
+    if (fileList[i] instanceof File && !fileList[i].hidden) {
+        var doc = open(fileList[i]);
 
-    // check object
-    if (fileList[i] instanceof File && fileList[i].hidden == false) {
-        var inumber =i+1;
-        // lert("#"+inumber+ "file is ready, press ok to continue", "file check", false);
-    }else{
-        continue;
+        app.backgroundColor.rgb.hexValue = 'ffffff';
+        app.preferences.rulerUnits = Units.PIXELS;
+
+        var picw = doc.width.value;
+        var pich = doc.height.value;
+        var picname = doc.name.split('.')[0];
+
+        var a = 1.1;
+        var cw = picw + (a - 1) * picw;
+        var ch1 = pich + (a - 1) * pich;
+
+        if (picw > pich) {
+            ch1 = cw;
+        } else {
+            cw = ch1;
         }
 
-    var doc = open(fileList[i]);
+        doc.resizeImage(picw, pich, 300, ResampleMethod.AUTOMATIC, 100);
+        doc.resizeCanvas(cw, ch1, AnchorPosition.MIDDLECENTER);
 
-    // set background color
-    app.backgroundColor.rgb.hexValue = 'ffffff';
-    // set unit
-    app.preferences.rulerUnits = Units.PIXELS
+        // Flatten the image to reduce memory usage
+        doc.flatten();
 
-    var picw = doc.width.value;
-    var pich = doc.height.value;
-
-    // get individual file name
-    var picname = doc.name.split ('.')[0];
-
-    gapsample = picw
-    var a= 1.1 // here a is the ratio of frame
-    var cw = picw+(a-1)*gapsample
-    var ch1 = pich+(a-1)*gapsample
-
-    // make background square
-    if (picw > pich){
-        ch1 = cw
-    }else{
-        cw = ch1
+        var fileOut = new File(outputFolder + '/' + picname + ".jpg");
+        var myoptions = new JPEGSaveOptions();
+        myoptions.quality = 12;
+        doc.saveAs(fileOut, myoptions, true, Extension.LOWERCASE);
+        
+        // Close the document to free up memory
+        doc.close(SaveOptions.DONOTSAVECHANGES);
     }
-    
-    // resolution after adjustment
-    var resolution = 300;
-    // resample method be automatic
-    var resampleMethod = ResampleMethod.AUTOMATIC;
-    // noise after adjustment
-    var amount = 100;
-    // reset size
-    doc.resizeImage(picw, pich, resolution, resampleMethod, amount);
-
-    // set anchor position
-    var anchor1 = AnchorPosition.MIDDLECENTER;
-    // resize canvas
-    doc.resizeCanvas(cw, ch1, anchor1);
-
-    // file saving path
-    var fileOut = new File(outputFolder+'//'+picname+".jpg");
-    // save as jpeg
-    var myoptions = JPEGSaveOptions;
-    myoptions.quality=12;
-    // save as copy
-    var asCopy = true;
-    // set the extension as .jpg
-    var extensionType = Extension.LOWERCASE;
-    // save as the method
-    doc.saveAs(fileOut, myoptions, asCopy, extensionType);
-    doc.close (SaveOptions.DONOTSAVECHANGES)
 }
+
 alert("All done!", "Happy posting :)", false);
